19ba4c61a1181c245e455737cfe5084f578b27d3
maiklshetinin <sh_chetinin@mail.ru>
2022-01-04 12:06:22 +0300

    fix: fix function reverse

--- src/test.js
+++ src/test.js
@@ -1,10 +1,6 @@
 function reverse(number){
     let result=number.toString().split('').reverse().join('')
 
-return console.log(Number(result))
+return (Number(result))
 }
 
-reverse(123); // 321
-reverse(233); // 332
-reverse(535); // 535
-reverse(95034); // 43059
