df8d23b3b148fe1eea1c8f6edd7e3c33938d3b07
maiklshetinin <sh_chetinin@mail.ru>
2022-01-04 12:15:57 +0300

    feat: add reverse

--- src/test.js
+++ src/test.js
@@ -1,6 +1,10 @@
 function reverse(number){
     let result=number.toString().split('').reverse().join('')
 
-return (Number(result))
+return  (Number(result))
 }
 
+reverse(123); // 321
+reverse(233); // 332
+reverse(535); // 535
+reverse(95034)
